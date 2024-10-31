a2651abe43be6b5b8e0fc33924df15db84695d30
nazliozbekk <nazliozbek14@gmail.com>
2024-11-01 00:06:03 +0300

    final version

--- SIR_Improved/SIR_Model.py
+++ SIR_Improved/SIR_Model.py
@@ -7,19 +7,19 @@ import matplotlib.pyplot as plt
 # Sab & Iab & Rab >> SIR for AB
 # Sba & Iba & Rba >> SIR for BA
 # Ka & Kb >> transmission coefficients for group A and B (will be taken different)
-# Ra & Rb >> recovery rates for A and B
+# Reca & Recb >> recovery rates for A and B
 # Mab >> the proportion of A that travels to B / stay during Dab weeks
 # Mba >> the proportion of B that travels to A / stay during Dba weeks
 # weeks >> duration of the disease
 
-def SIR_Model(Na, Nb, Ka, Kb, Ra, Rb, Mab, Mba, Dab, Dba, weeks):
+def SIR_Model(Na, Nb, Ka, Kb, Reca, Recb, Mab, Mba, Dab, Dba, weeks):
     Sa = np.empty(weeks + 1)
     Ia = np.empty(weeks + 1)
-    Ra_ = np.empty(weeks + 1)
+    Ra = np.empty(weeks + 1)
 
     Sb = np.empty(weeks + 1)
     Ib = np.empty(weeks + 1)
-    Rb_ = np.empty(weeks + 1)
+    Rb = np.empty(weeks + 1)
 
     Sab = np.empty(weeks + 1)
     Iab = np.empty(weeks + 1)
@@ -29,66 +29,60 @@ def SIR_Model(Na, Nb, Ka, Kb, Ra, Rb, Mab, Mba, Dab, Dba, weeks):
     Iba = np.empty(weeks + 1)
     Rba = np.empty(weeks + 1)
 
-    # Initial conditions
     Sa[0] = Na * 0.99
     Ia[0] = Na * 0.01
-    Ra_[0] = 0
+    Ra[0] = 0
 
     Sb[0] = Nb
     Ib[0] = 0
-    Rb_[0] = 0
+    Rb[0] = 0
 
-    # Simulation loop
     for t in range(1, weeks + 1):
         inflow_Sab = Mab * Sa[t - 1] / Dab
         inflow_Iab = Mab * Ia[t - 1] / Dab
-        inflow_Rab = Mab * Ra_[t - 1] / Dab
+        inflow_Rab = Mab * Ra[t - 1] / Dab
 
         inflow_Sba = Mba * Sb[t - 1] / Dba
         inflow_Iba = Mba * Ib[t - 1] / Dba
-        inflow_Rba = Mba * Rb_[t - 1] / Dba
+        inflow_Rba = Mba * Rb[t - 1] / Dba
 
         outflow_Sa = Mab * Sa[t - 1] / Dab
         outflow_Ia = Mab * Ia[t - 1] / Dab
-        outflow_Ra = Mab * Ra_[t - 1] / Dab
+        outflow_Ra = Mab * Ra[t - 1] / Dab
 
         outflow_Sb = Mba * Sb[t - 1] / Dba
         outflow_Ib = Mba * Ib[t - 1] / Dba
-        outflow_Rb = Mba * Rb_[t - 1] / Dba
+        outflow_Rb = Mba * Rb[t - 1] / Dba
 
         # A
         Sa[t] = max(0, min(Na, Sa[t - 1] - Ka * Sa[t - 1] * Ia[t - 1] + inflow_Sba - outflow_Sa))
-        Ia[t] = max(0, min(Na - Sa[t] - Ra_[t], Ia[t - 1] + Ka * Sa[t - 1] * Ia[t - 1] - Ra * Ia[t - 1] + inflow_Iba - outflow_Ia))
-        Ra_[t] = max(0, min(Na - Sa[t] - Ia[t], Ra_[t - 1] + Ra * Ia[t - 1] + inflow_Rba - outflow_Ra))
+        Ia[t] = max(0, min(Na - Sa[t] - Ra[t], Ia[t - 1] + Ka * Sa[t - 1] * Ia[t - 1] - Reca * Ia[t - 1] + inflow_Iba - outflow_Ia))
+        Ra[t] = max(0, min(Na - Sa[t] - Ia[t], Ra[t - 1] + Reca * Ia[t - 1] + inflow_Rba - outflow_Ra))
 
         # B
         Sb[t] = max(0, min(Nb, Sb[t - 1] - Kb * Sb[t - 1] * Ib[t - 1] + inflow_Sab - outflow_Sb))
-        Ib[t] = max(0, min(Nb - Sb[t] - Rb_[t], Ib[t - 1] + Kb * Sb[t - 1] * Ib[t - 1] - Rb * Ib[t - 1] + inflow_Iab - outflow_Ib))
-        Rb_[t] = max(0, min(Nb - Sb[t] - Ib[t], Rb_[t - 1] + Rb * Ib[t - 1] + inflow_Rab - outflow_Rb))
+        Ib[t] = max(0, min(Nb - Sb[t] - Rb[t], Ib[t - 1] + Kb * Sb[t - 1] * Ib[t - 1] - Recb * Ib[t - 1] + inflow_Iab - outflow_Ib))
+        Rb[t] = max(0, min(Nb - Sb[t] - Ib[t], Rb[t - 1] + Recb * Ib[t - 1] + inflow_Rab - outflow_Rb))
 
         # AB
         Sab[t] = max(0, inflow_Sab - Kb * Sab[t - 1] * Ib[t - 1])
-        Iab[t] = max(0, inflow_Iab + Kb * Sab[t - 1] * Ib[t - 1] - Rb * Iab[t - 1])
-        Rab[t] = max(0, inflow_Rab + Rb * Iab[t - 1])
+        Iab[t] = max(0, inflow_Iab + Kb * Sab[t - 1] * Ib[t - 1] - Recb * Iab[t - 1])
+        Rab[t] = max(0, inflow_Rab + Recb * Iab[t - 1])
 
         # BA
         Sba[t] = max(0, inflow_Sba - Ka * Sba[t - 1] * Ia[t - 1])
-        Iba[t] = max(0, inflow_Iba + Ka * Sba[t - 1] * Ia[t - 1] - Ra * Iba[t - 1])
-        Rba[t] = max(0, inflow_Rba + Ra * Iba[t - 1])
+        Iba[t] = max(0, inflow_Iba + Ka * Sba[t - 1] * Ia[t - 1] - Reca * Iba[t - 1])
+        Rba[t] = max(0, inflow_Rba + Reca * Iba[t - 1])
 
-        # Print total populations to check for issues
-        print(Sa[t] + Ia[t] + Ra_[t] + Sb[t] + Ib[t] + Rb_[t] + Sab[t] + Iab[t] + Rab[t] + Sba[t] + Iba[t] + Rba[t])
 
-    # Generate time axis for plotting
     time = np.arange(weeks + 1)
 
-    # Create plots
-    fig, axs = plt.subplots(2, 2, figsize=(18, 12))  # 2x2 grid, larger figure size
+    fig, axs = plt.subplots(2, 2, figsize=(18, 12))
 
     # Plot 1: Group A (Local Population)
     axs[0, 0].plot(time, Sa, label='Susceptible', color='blue')
     axs[0, 0].plot(time, Ia, label='Infected', color='red')
-    axs[0, 0].plot(time, Ra_, label='Recovered', color='green')
+    axs[0, 0].plot(time, Ra, label='Recovered', color='green')
     axs[0, 0].set_title('Group A')
     axs[0, 0].set_xlabel('Weeks')
     axs[0, 0].set_ylabel('Population')
@@ -97,7 +91,7 @@ def SIR_Model(Na, Nb, Ka, Kb, Ra, Rb, Mab, Mba, Dab, Dba, weeks):
     # Plot 2: Group B (Local Population)
     axs[0, 1].plot(time, Sb, label='Susceptible', color='blue')
     axs[0, 1].plot(time, Ib, label='Infected', color='red')
-    axs[0, 1].plot(time, Rb_, label='Recovered', color='green')
+    axs[0, 1].plot(time, Rb, label='Recovered', color='green')
     axs[0, 1].set_title('Group B')
     axs[0, 1].set_xlabel('Weeks')
     axs[0, 1].set_ylabel('Population')
@@ -121,14 +115,15 @@ def SIR_Model(Na, Nb, Ka, Kb, Ra, Rb, Mab, Mba, Dab, Dba, weeks):
     axs[1, 1].set_ylabel('Population')
     axs[1, 1].legend()
 
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
     Na, Nb = 1000, 800
     Ka, Kb = 0.002, 0.001
-    Ra, Rb = 0.1, 0.05
+    Reca, Recb = 0.1, 0.05
     Mab, Mba = 0.05, 0.05
     Dab, Dba = 3, 3
-    weeks = 100
+    weeks = 16
 
-    SIR_Model(Na, Nb, Ka, Kb, Ra, Rb, Mab, Mba, Dab, Dba, weeks)
+    SIR_Model(Na, Nb, Ka, Kb, Reca, Recb, Mab, Mba, Dab, Dba, weeks)
